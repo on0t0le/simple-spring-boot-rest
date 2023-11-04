ad5e04abb1a08284c5d83cd7dff969796595786f
anatolii.b <anatolii.b@opsworks.co>
2023-11-04 22:28:07 +0200

    Initialized project

---
+++ src/test/java/com/onotole/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onotole.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
